01ed5a1b6a4474eb36ff889332f5726a70af2f40
Igor Amoras <igorduarteamoras@gmail.com>
2022-02-24 16:05:23 -0300

    wip

--- dist/watermark.js
+++ dist/watermark.js
@@ -1,3 +1,4 @@
+import window from "global";
 (function webpackUniversalModuleDefinition(root, factory) {
   if (typeof exports === "object" && typeof module === "object")
     module.exports = factory();
